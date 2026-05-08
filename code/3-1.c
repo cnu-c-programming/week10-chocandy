7a90c0fa3952d6414dc7e29d3247bb9e90dbd661
chominjeong <chomj4836@gmail.com>
2026-05-08 11:17:46 +0900

    Update 3-1

---
+++ code/3-1.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student {
+    char name[100];
+    int score;
+    struct Student* next;
+};
+
+void add_student(struct Student** head, char name[], int score) {
+    struct Student* new_node = malloc(sizeof(struct Student));
+
+    strcpy(new_node->name, name);
+    new_node->score = score;
+    new_node->next = NULL;
+
+    if (*head == NULL) {
+        *head = new_node;
+        return;
+    }
+
+    struct Student* cur = *head;
+
+    while (cur->next != NULL) {
+        cur = cur->next;
+    }
+
+    cur->next = new_node;
+}
+
+void delete_student(struct Student** head, char name[]) {
+    struct Student* cur = *head;
+    struct Student* prev = NULL;
+
+    while (cur != NULL) {
+        if (strcmp(cur->name, name) == 0) {
+            if (prev == NULL) {
+                *head = cur->next;
+            } else {
+                prev->next = cur->next;
+            }
+
+            free(cur);
+            return;
+        }
+
+        prev = cur;
+        cur = cur->next;
+    }
+}
+
+void print_list(struct Student* head) {
+    struct Student* cur = head;
+
+    while (cur != NULL) {
+        printf("%s %d\n", cur->name, cur->score);
+        cur = cur->next;
+    }
+}
+
+void free_list(struct Student* head) {
+    struct Student* cur = head;
+
+    while (cur != NULL) {
+        struct Student* temp = cur;
+        cur = cur->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct Student* head = NULL;
+
+    char command[20];
+    char name[100];
+    int score;
+
+    while (1) {
+        scanf("%s", command);
+
+        if (strcmp(command, "add") == 0) {
+            scanf("%s %d", name, &score);
+            add_student(&head, name, score);
+        } else if (strcmp(command, "delete") == 0) {
+            scanf("%s", name);
+            delete_student(&head, name);
+        } else if (strcmp(command, "print") == 0) {
+            print_list(head);
+        } else if (strcmp(command, "quit") == 0) {
+            free_list(head);
+            break;
+        }
+    }
+
+    return 0;
+}
